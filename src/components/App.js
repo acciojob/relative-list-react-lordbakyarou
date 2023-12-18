044d323b698a28fd344c8463dc111de879f79273
Mayur Hanwate <frostblizard111@gmail.com>
2023-12-19 02:20:15 +0530

    g

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
 
   return (
     <div id="main">
-      <ol id="#relativeList">
+      <ol id="relativeList">
         {data.map((item, index) => {
           return (
             <li
